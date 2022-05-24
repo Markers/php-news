b0be66c330d39e973fa2a7bcfebc66bdc536b464
Kyungseo-Park <troublesome.dev@gmail.com>
2022-05-25 01:35:57 +0900

    feat: footer icon 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
+import { useEffect, useState } from "react";
+
 import Head from "next/head";
 import Navbar from "pagesComponents/Navbar";
 import Header from "pagesComponents/Header";
@@ -6,14 +8,6 @@ import Footer from "pagesComponents/Footer";
 import { Card } from "../src";
 
 // sections
-import SectionFeatures from "pagesComponents/Sections/SectionFeatures";
-import SectionButton from "pagesComponents/Sections/SectionButton";
-import SectionColorsPalette from "pagesComponents/Sections/SectionColorsPalette";
-import SectionShadow from "pagesComponents/Sections/SectionShadow";
-import SectionComponents from "pagesComponents/Sections/SectionComponents";
-import SectionFramework from "pagesComponents/Sections/SectionFramework";
-import SectionTestimonials from "pagesComponents/Sections/SectionTestimonials";
-import SectionCommunity from "pagesComponents/Sections/SectionCommunity";
 
 export default function Presentation() {
   return (
@@ -25,24 +19,11 @@ export default function Presentation() {
         <meta name="keywords" content="php, laravel, ci, php annotated, php annotated monthly" />
       </Head>
       <Navbar shadow />
-      <main className="relative">
-        <Header />
+      <main className="relative pt-4">
         <Card shadow={false} className="bg-white -mt-20 md:-mt-48 mx-6 md:mx-12">
           <div className="container mx-auto px-4 z-20">
-            <SectionFeatures />
-            <SectionButton />
-            <SectionColorsPalette />
-            <SectionShadow />
-            <SectionComponents />
-            <SectionFramework />
-            <SectionTestimonials />
-            <SectionCommunity />
+            <span>ds</span>
           </div>
-          <img
-            className="absolute w-full bottom-0 md:-bottom-40"
-            src="/img/pre-footer.jpg"
-            alt="bubbles"
-          />
         </Card>
       </main>
       <Footer />
---
+++ utils/social.ts
@@ -0,0 +1,25 @@
+// href = "https://www.twitter.com/creativetim?ref=material-tailwind";
+// target = "_blank";
+// rel = "noreferrer";
+export const socialList = [
+  {
+    href: "https://www.twitter.com/creativetim?ref=material-tailwind",
+    id: 1,
+    icon: `fab fa-twitter bg-white text-light-blue-400 shadow-lg font-lg p-3 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block text-center`,
+  },
+  {
+    href: "https://www.facebook.com/creativetim?ref=material-tailwind",
+    id: 2,
+    icon: `fab fa-facebook-square bg-white text-light-blue-600 shadow-lg font-lg p-3 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block text-center`,
+  },
+  {
+    href: "https://www.twitter.com/creativetim?ref=material-tailwind",
+    id: 3,
+    icon: `fab fa-dribbble bg-white text-pink-400 shadow-lg font-lg p-3 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block text-center`,
+  },
+  {
+    href: "https://www.twitter.com/creativetim?ref=material-tailwind",
+    id: 4,
+    icon: `fab fa-discord bg-white text-blue-700 shadow-lg font-lg py-3 px-2.5 items-center justify-center align-center rounded-full outline-none focus:outline-none inline-block text-center`,
+  },
+];
