997072f4e1224bfa5b72a994a55e106efb7b4435
Kyoungseo Park <45850400+Kyungseo-Park@users.noreply.github.com>
2022-06-30 13:15:36 +0900

    feat: update articleCard component

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,12 +1,12 @@
 const ArticleCard = ({ value }: any) => {
   return (
     <a href={value?.slug}>
-      <article className="mx-auto group w-full shadow-2xl max-w-md pb-8 rounded-b-2xl transform duration-500 hover:-translate-y-2 cursor-pointer">
+      <article className="mx-auto group w-full shadow-2xl max-w-md pb-8 rounded-b-2xl transform duration-500 hover:-translate-y-2 cursor-pointer" style={{height: '100%'}}>
         <img src={value.thumbnail} />
         <div className="mt-14 px-4">
-          <span>Date {value.translated_title}</span>
-          <h2 className="mt-4 text-base font-medium text-gray-400">{value.translated_title}</h2>
-          <p className="mt-2 text-2xl text-gray-700">{value.translated_description}</p>
+          <span>{value.publish_date}</span>
+          <h2 className="mt-4 text-2xl text-base font-medium text-black-400">{value.translated_title}</h2>
+          <p className="mt-2 text-gray-700">{value.translated_description}</p>
         </div>
       </article>
     </a>
