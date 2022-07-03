6ff36f38dcbfea99d985e527ea1ac484b89d80e0
Kyungseo-Park <troublesome.dev@gmail.com>
2022-07-04 02:24:07 +0900

    feat: jetbrains 에 대한 todolist 추가함

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
@@ -10,6 +10,16 @@ const nextConfig = {
       "avatars.githubusercontent.com",
     ],
   },
+  // jetbrains 요청을 => next.js 의 api 로 프록시 보내고
+  // api 에서 jetbrains 으로 재요청 해야함
+  // async rewrites() {
+  //   return [
+  //     {
+  //       destination: 'https://*.jetbrains.com/',
+  //       source: '/api/jetbrains',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
