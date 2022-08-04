045e354c10e9a7b1655b5df70f764a77b6e2d14f
Kyungseo-Park <troublesome.dev@gmail.com>
2022-08-05 01:25:00 +0900

    :bulb: 불필요한 style 삭제 그리고  통합

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -3,4 +3,31 @@ import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
     ${normalize}
+    html,
+    body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
+
+    * {
+    box-sizing: border-box;
+    }
+
+    @media (prefers-color-scheme: dark) {
+    html {
+        color-scheme: dark;
+    }
+    body {
+        color: white;
+        background: black;
+    }
+    }
+
 `;
