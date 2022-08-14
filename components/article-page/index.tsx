6b0dc2575aef2decd02a9d3542843002d563a9ca
Kyungseo-Park <troublesome.dev@gmail.com>
2022-08-14 23:28:01 +0900

    :wrench: article 에 입력 되어야 할 내용 추가

--- components/article-page/index.tsx
+++ components/article-page/index.tsx
@@ -1,6 +1,28 @@
 import { ArticlePageProps } from "@/pages/[category]/[postId]";
-import { Markdown } from "./styled";
+import { MardkownHeader, Markdown } from "./styled";
 
 export const ArticlePage = ({ article, content }: ArticlePageProps) => {
-  return <Markdown dangerouslySetInnerHTML={{ __html: content }} />;
+  console.log(article);
+
+  return (
+    <>
+      <MardkownHeader className="header">
+        <div className="태그"></div>
+        <div className="아코디언">
+          <span>Home</span>
+          <span>{article.data.category}</span>
+          <span>{article.data.post_id}</span>
+        </div>
+        <div className="제목">{article.data.translated_title}</div>
+        <div className="주석">
+          <span>
+            {/* 작가 */} {article.data.author}
+            {/* 원본 링크 */}
+            {/* 공개 일정 등 */} {article.data.publish_date}
+          </span>
+        </div>
+      </MardkownHeader>
+      <Markdown dangerouslySetInnerHTML={{ __html: content }} />
+    </>
+  );
 };
--- components/article-page/styled.ts
+++ components/article-page/styled.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+export const MardkownHeader = styled.div``;
+
 export const Markdown = styled.article`
   @media print {
     *,
