a1a0b2d12ca9f7a22c7e000fbcccf13d795f4f40
Kyungseo-Park <troublesome.dev@gmail.com>
2022-08-17 01:24:03 +0900

    :stethoscope: 상세 페이지 CSS 추가함

--- components/article-page/index.tsx
+++ components/article-page/index.tsx
@@ -1,28 +1,30 @@
 import { ArticlePageProps } from "@/pages/[category]/[postId]";
-import { MardkownHeader, Markdown } from "./styled";
+import { MardkownHeader, Markdown, TestLayout } from "./styled";
 
 export const ArticlePage = ({ article, content }: ArticlePageProps) => {
   console.log(article);
 
   return (
     <>
-      <MardkownHeader className="header">
-        <div className="태그"></div>
-        <div className="아코디언">
-          <span>Home</span>
-          <span>{article.data.category}</span>
-          <span>{article.data.post_id}</span>
-        </div>
-        <div className="제목">{article.data.translated_title}</div>
-        <div className="주석">
-          <span>
-            {/* 작가 */} {article.data.author}
-            {/* 원본 링크 */}
-            {/* 공개 일정 등 */} {article.data.publish_date}
-          </span>
-        </div>
-      </MardkownHeader>
-      <Markdown dangerouslySetInnerHTML={{ __html: content }} />
+      <TestLayout>
+        <MardkownHeader className="header">
+          <div className="태그"></div>
+          <div className="아코디언">
+            <span>Home</span>
+            <span>{article.data.category}</span>
+            <span>{article.data.post_id}</span>
+          </div>
+          <div className="제목">{article.data.translated_title}</div>
+          <div className="주석">
+            <span>
+              {/* 작가 */} {article.data.author}
+              {/* 원본 링크 */}
+              {/* 공개 일정 등 */} {article.data.publish_date}
+            </span>
+          </div>
+        </MardkownHeader>
+        <Markdown dangerouslySetInnerHTML={{ __html: content }} />
+      </TestLayout>
     </>
   );
 };
--- components/article-page/styled.ts
+++ components/article-page/styled.ts
@@ -1,187 +1,73 @@
 import styled from "styled-components";
-
+export const TestLayout = styled.div`
+  max-width: 800px;
+  margin: auto;
+`;
 export const MardkownHeader = styled.div``;
 
 export const Markdown = styled.article`
-  @media print {
-    *,
-    *:before,
-    *:after {
-      background: transparent !important;
-      color: #000 !important;
-      box-shadow: none !important;
-      text-shadow: none !important;
-    }
-    a,
-    a:visited {
-      text-decoration: underline;
-    }
-    a[href]:after {
-      content: " (" attr(href) ")";
-    }
-    abbr[title]:after {
-      content: " (" attr(title) ")";
-    }
-    a[href^="#"]:after,
-    a[href^="javascript:"]:after {
-      content: "";
-    }
-    pre,
-    blockquote {
-      border: 1px solid #999;
-      page-break-inside: avoid;
-    }
-    thead {
-      display: table-header-group;
-    }
-    tr,
-    img {
-      page-break-inside: avoid;
-    }
-    img {
-      max-width: 100% !important;
-    }
-    p,
-    h2,
-    h3 {
-      orphans: 3;
-      widows: 3;
-    }
-    h2,
-    h3 {
-      page-break-after: avoid;
+  h1 {
+    font-size: 1.8rem;
+  }
+  p {
+    font-size: 1.4rem;
+  }
+  > ul > li:before {
+    content: counter(item);
+    display: inline-block;
+    height: 20px;
+    margin-right: 5px;
+    border: 1px solid #000;
+    color: #000;
+    font-size: 20px;
+    padding: 0 8px 8px;
+    border-radius: 3px;
+    line-height: 1.3;
+  }
+  > ul {
+    line-height: 2;
+    > li {
+      list-style-type: none;
+      counter-increment: item;
+
+      blockquote {
+        margin: auto;
+        padding: 0px;
+        a {
+          padding: 0px;
+        }
+        p {
+          font-size: 16px;
+        }
+      }
+
+      ul li {
+        list-style-type: upper-roman;
+        counter-increment: none;
+      }
+
+      ul li:before {
+        /* content: counter(item2); */
+      }
     }
   }
-  pre,
-  code {
-    font-family: Menlo, Monaco, "Courier New", monospace;
+
+  img {
+    width: 100%;
   }
-  pre {
-    padding: 0.5rem;
-    line-height: 1.25;
-    overflow-x: scroll;
+  a {
+    text-decoration: underline;
+    color: #868686;
   }
-  a,
-  a:visited {
-    color: #3498db;
+
+  /* visited link */
+  /* mouse over link */
+  a:hover {
+    color: #060606;
   }
-  a:hover,
-  a:focus,
+
+  /* selected link */
   a:active {
-    color: #2980b9;
-  }
-  .modest-no-decoration {
-    text-decoration: none;
-  }
-  html {
-    font-size: 12px;
-  }
-  @media screen and (min-width: 32rem) and (max-width: 48rem) {
-    html {
-      font-size: 15px;
-    }
-  }
-  @media screen and (min-width: 48rem) {
-    html {
-      font-size: 16px;
-    }
-  }
-  body {
-    line-height: 1.85;
-  }
-  p,
-  .modest-p {
-    font-size: 1rem;
-    margin-bottom: 1.3rem;
-  }
-  h1,
-  .modest-h1,
-  h2,
-  .modest-h2,
-  h3,
-  .modest-h3,
-  h4,
-  .modest-h4 {
-    margin: 1.414rem 0 0.5rem;
-    font-weight: inherit;
-    line-height: 1.42;
-  }
-  h1,
-  .modest-h1 {
-    margin-top: 0;
-    font-size: 3.998rem;
-  }
-  h2,
-  .modest-h2 {
-    font-size: 2.827rem;
-  }
-  h3,
-  .modest-h3 {
-    font-size: 1.999rem;
-  }
-  h4,
-  .modest-h4 {
-    font-size: 1.414rem;
-  }
-  h5,
-  .modest-h5 {
-    font-size: 1.121rem;
-  }
-  h6,
-  .modest-h6 {
-    font-size: 0.88rem;
-  }
-  small,
-  .modest-small {
-    font-size: 0.707em;
-  }
-  /* https://github.com/mrmrs/fluidity */
-  img,
-  canvas,
-  iframe,
-  video,
-  svg,
-  select,
-  textarea {
-    max-width: 100%;
-  }
-  @import url(http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700);
-  @import url(http://fonts.googleapis.com/css?family=Arimo:700,700italic);
-  html {
-    font-size: 18px;
-    max-width: 100%;
-  }
-  body {
-    color: #444;
-    font-family: "Open Sans Condensed", sans-serif;
-    font-weight: 300;
-    margin: 0 auto;
-    max-width: 48rem;
-    line-height: 1.45;
-    padding: 0.25rem;
-  }
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: Arimo, Helvetica, sans-serif;
-  }
-  h1,
-  h2,
-  h3 {
-    border-bottom: 2px solid #fafafa;
-    margin-bottom: 1.15rem;
-    padding-bottom: 0.5rem;
-    text-align: center;
-  }
-  blockquote {
-    border-left: 8px solid #fafafa;
-    padding: 1rem;
-  }
-  pre,
-  code {
-    background-color: #fafafa;
+    color: #000;
   }
 `;
